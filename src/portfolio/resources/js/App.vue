fa6ee1cb5ca0b1ba906e553370132988c0064536
goldenslumber <goldenslumbers0902@gmail.com>
2022-05-07 00:47:50 +0900

    Vueの環境構築

---
+++ src/portfolio/resources/js/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-container>
+                <RouterView />
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
--- src/portfolio/resources/js/app.js
+++ src/portfolio/resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+import Vue from 'vue'
+import router from './router'
+import App from './App.vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify);
+
+new Vue({
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App />',
+    vuetify: new Vuetify()
+})
