00c5f595e2d2facc020e571265bfc1cca09d48d8
goldenslumber <goldenslumbers0902@gmail.com>
2022-05-07 20:45:29 +0900

    #5 DBテーブル作成

---
+++ src/portfolio/database/migrations/2022_05_07_103304_create_broadcast_areas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('broadcast_areas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('broadcaster_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('prefecture_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('broadcast_areas');
+    }
+};
