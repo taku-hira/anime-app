b3c1bd70cb8cac4e45d5e61eb4843abf777d6a7e
goldenslumber <goldenslumbers0902@gmail.com>
2022-06-03 23:07:00 +0900

    テストページ表示

--- src/api/routes/web.php
+++ src/api/routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/scraping', [ScrapingController::class, 'getAnimeData']);
