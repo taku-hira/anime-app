3a5235b8013c94db0ffd65a3ae7435ce57c81e22
goldenslumber <goldenslumbers0902@gmail.com>
2022-06-10 08:07:15 +0900

    #39 LoginController編集

--- src/api/app/Http/Controllers/Auth/LoginController.php
+++ src/api/app/Http/Controllers/Auth/LoginController.php
@@ -4,8 +4,23 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\JsonResponse;
 
 class LoginController extends Controller
 {
-    
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return response()->json(['name' => Auth::user()->name], 200);
+        } else {
+            return response()->json('ログイン失敗しました', 401);
+        }
+    }
 }
