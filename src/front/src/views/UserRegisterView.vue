25a1281acb8cc873cbfa5cf508ff18f40a183d9e
goldenslumber <goldenslumbers0902@gmail.com>
2022-06-12 00:03:36 +0900

    #41 Vueの登録画面大枠

---
+++ src/front/src/views/UserRegisterView.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-card width="400px" class="mx-auto mt-5">
+        <v-card-title>
+          <h1 class="display-1">Let's Sign UP!</h1>
+        </v-card-title>
+        <v-card-text>
+          <form @submit.prevent="login">
+            <v-text-field
+              v-model="input.name"
+              label="name"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="input.email"
+              label="e-mail"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="input.prefecture_id"
+              label="都道府県"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="input.password"
+              label="password"
+              type="password"
+              required
+            ></v-text-field>
+            <v-btn
+              class="mr-4"
+              type="submit"
+            >
+              Sign up
+            </v-btn>
+          </form>
+        </v-card-text>
+      </v-card>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Sign up'
+    },
+  data(){
+    return {
+      input: {
+        'name': '',
+        'email': '',
+        'prefecture_id': '',
+        'password': '',
+      },
+      status: '',
+      message: '',
+      user: '',
+    }
+  },
+}
+</script>
