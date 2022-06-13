51d45f57c20cb2fb2834d0b8665bb3127ce7594d
goldenslumber <goldenslumbers0902@gmail.com>
2022-06-13 23:22:46 +0900

    #44 ScrapingController リファクタリング

--- src/api/app/Http/Controllers/ScrapingController.php
+++ src/api/app/Http/Controllers/ScrapingController.php
@@ -13,41 +13,66 @@ class ScrapingController extends Controller
     {
         $client = new Client();
         $client = new Client(HttpClient::create(['timeout' => 60]));
-        //詳細ページのURL取得
+        $anime_url = $this->getUrl($client);
+        foreach ($anime_url as $url) {
+            $crawler = $client->request('GET', $url);
+            $anime_title = $this->getTitle($crawler);
+            $anime_summary = $this->getSummary($crawler);
+            $anime_img_url = $this->getImageUrl($crawler);
+
+            $anime_data[] = [
+                'title' => $anime_title,
+                'summary' => $anime_summary,
+                'img_url' => $anime_img_url,
+            ];
+        }
+        $this->insertAnimeDb($anime_data);
+    }
+
+    public function getUrl($client)
+    {
         $crawler = $client->request('GET', 'https://anime.eiga.com/program/');
         $anime_url = $crawler->filter('.animeSeasonBox')->each(function ($element) {
             return 'https://anime.eiga.com' . $element->filter('a')->attr('href');
         });
-        foreach ($anime_url as $url) {
-            $crawler = $client->request('GET', $url);
-            // タイトル取得
-            $anime_title = $crawler->filter('.headArea h1')->each(function ($element) {
-                if ($element->filter('.headTtlL')->count() > 0) {
-                    return $element->filter('.headTtlL')->text();
-                }
-            });
-            // あらすじ取得
-            $anime_summary = $crawler->filter('.animeTopContainer')->each(function ($element) {
-                if ($element->filter('.animeDetailBox')->filter('dl#detailSynopsis')->filter('dd')->count() > 0) {
-                    return $element->filter('.animeDetailBox')->filter('dl#detailSynopsis')->filter('dd')->text();
-                } else {
-                    return $element->filter('.animeDetailBox')->filter('dl#detailLink')
-                                    ->filter('dd')->filter('.clearfix a')->attr('href');
-                }
-            });
-            // 画像URL取得
-            $anime_img = $crawler->filter('.animeTopContainer')->each(function ($element) {
-                if ($element->filter('.animeDetailBox')->filter('.animeDetailImg img')->count() > 0) {
-                    return $element->filter('.animeDetailBox')->filter('.animeDetailImg img')->attr('src');
-                }
-            });
-            $anime_data[] = [
-                'title' => $anime_title,
-                'summary' => $anime_summary,
-                'img' =>  $anime_img,
-            ];
-        }
-        // 取得したデータをanimesテーブルに保存
+        return $anime_url;
+    }
+
+    public function getTitle($crawler)
+    {
+        $anime_title = $crawler->filter('.headArea h1')->each(function ($element) {
+            if ($element->filter('.headTtlL')->count() > 0) {
+                return $element->filter('.headTtlL')->text();
+            }
+        });
+        return $anime_title;
+    }
+
+    public function getSummary($crawler)
+    {
+        $anime_summary = $crawler->filter('.animeTopContainer')->each(function ($element) {
+            if ($element->filter('.animeDetailBox')->filter('dl#detailSynopsis')->filter('dd')->count() > 0) {
+                return $element->filter('.animeDetailBox')->filter('dl#detailSynopsis')->filter('dd')->text();
+            } else {
+                return $element->filter('.animeDetailBox')->filter('dl#detailLink')
+                                ->filter('dd')->filter('.clearfix a')->attr('href');
+            }
+        });
+        return $anime_summary;
+    }
+
+    public function getImageUrl($crawler)
+    {
+        $anime_img_url = $crawler->filter('.animeTopContainer')->each(function ($element) {
+            if ($element->filter('.animeDetailBox')->filter('.animeDetailImg img')->count() > 0) {
+                return $element->filter('.animeDetailBox')->filter('.animeDetailImg img')->attr('src');
+            }
+        });
+        return $anime_img_url;
+    }
+
+    public function insertAnimeDb($anime_data)
+    {
         foreach ($anime_data as $data) {
             $anime = new Anime();
             $anime->title = $data['title'][0];
