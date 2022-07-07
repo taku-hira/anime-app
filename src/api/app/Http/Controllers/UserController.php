82629f451cedb80ccbcbdf197d95106220d61f0c
goldenslumber <goldenslumbers0902@gmail.com>
2022-07-07 18:18:53 +0900

    ユーザーコントロラー editメソッド作成

--- src/api/app/Http/Controllers/UserController.php
+++ src/api/app/Http/Controllers/UserController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return response()->json($user, 200);
+    }
+
 }
