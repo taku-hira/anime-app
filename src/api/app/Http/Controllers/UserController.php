6b960afccfd8ae446f176c256d3b47b5a359a088
goldenslumber <goldenslumbers0902@gmail.com>
2022-07-07 19:06:33 +0900

    UserController updateメソッド作成

--- src/api/app/Http/Controllers/UserController.php
+++ src/api/app/Http/Controllers/UserController.php
@@ -14,4 +14,19 @@ class UserController extends Controller
         return response()->json($user, 200);
     }
 
+    public function update(Request $request)
+    {
+        $user = User::findOrFail(Auth::id());
+        $request->validate([
+            'name' => ['required', 'string', 'max:20'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'prefecture_id' => ['integer', 'required', 'min:1', 'max:47'],
+        ]);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->prefecture_id = $request->prefecture_id;
+        $user->save();
+
+        return response()->json($user,200);
+    }
 }
